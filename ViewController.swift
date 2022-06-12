6c9b2d8610414859e627c7aae759a86f8b8504c7
ALexeyKhilo <61181942+ALexeyKhilo@users.noreply.github.com>
2022-06-12 09:45:05 +0300

    Add files via upload

--- ViewController.swift
+++ ViewController.swift
@@ -1,52 +1,217 @@
 //
 //  ViewController.swift
-//  WeekdayFinder
+//  LearnKit
 //
-//  Created by Alexey Khilo on 2/11/22.
+//  Created by Alexey Khilo on 2/16/22.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+
+    var uiElements = ["UISegmentedControl",
+                      "UILabel",
+                      "UISlider",
+                      "UITextField",
+                      "UIButton",
+                      "UIDatePicker"]
+    
+    var selectedElement: String?
     
     
-    @IBOutlet weak var dateTF: UITextField!
-    @IBOutlet weak var monthTF: UITextField!
-    @IBOutlet weak var yearTF: UITextField!
-     
-    @IBOutlet weak var resaultLabel: UILabel!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var label: UILabel!
     
-    @IBAction func findDay(_ sender: UIButton) {
-      
-        let calendar = Calendar.current
-        
-        var dateComponents = DateComponents()
-        guard let day = dateTF.text, let month = monthTF.text, let year = yearTF.text  else { return }
-        dateComponents.day = Int(day)
-        dateComponents.month = Int(month)
-        dateComponents.year = Int(year)
-        
-        guard let date = calendar.date(from: dateComponents) else { return }
-        
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "EEEE"
-        
-        let weekday = dateFormatter.string(from: date)
-        
-        resaultLabel.text = weekday
-        
-    }
+    @IBOutlet weak var slider: UISlider!
+    
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var button: UIButton!
+    
+    @IBOutlet weak var switchLabel: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        slider.value = 1
+        label.text = String(slider.value)
+        label.font.withSize(35)
+        label.font.withSize(25)
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        
+        segmentControl.insertSegment(withTitle: "Third", at: 2, animated: true)
+        
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        slider.minimumTrackTintColor = .yellow
+        slider.maximumTrackTintColor = .red
+        slider.thumbTintColor = .blue
+        
+        button.backgroundColor = .systemBlue
+        button.setTitle("Done", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        
+        choiceUiElements()
+        
+    }
+    
+    func hideAllElements() {
+        segmentControl.isHidden = true
+        label.isHidden = true
+        slider.isHidden = true
+        button.isHidden = true
+        datePicker.isHidden = true
+    }
+    
+    func choiceUiElements () {
+        let elementPicker = UIPickerView()
+        elementPicker.delegate = self
+        
+        textField.inputView = elementPicker
     }
 
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
+    
+    func createToolbar() {
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done",
+                                         style: .plain,
+                                         target: self,
+                                         action: #selector(dismissKeyboard))
+        
+        toolbar.setItems([doneButton], animated: true)
+        toolbar.isUserInteractionEnabled = true
+        
+        textField.inputAccessoryView = toolbar
+        
+    }
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    @IBAction func choiseSegment(_ sender: UISegmentedControl) {
+        
+        label.isHidden = false
+        
+        switch segmentControl.selectedSegmentIndex {
+        case 0:
+            label.text = "The First segment is selected"
+            label.textColor = .red
+        case 1:
+            label.text = "The Second segment is selected"
+            label.textColor = .blue
+        case 2:
+            label.text = "The Third segment is selected"
+            label.textColor = .yellow
+        default:
+            print("Something Wrong!")
+        }
     }
     
     
     
+    @IBAction func sliderAction(_ sender: UISlider) {
+        label.text = String(sender.value)
+        
+        let backgroundColor = self.view.backgroundColor
+        self.view.backgroundColor = backgroundColor?.withAlphaComponent(CGFloat(sender.value))
+    }
+    
+    
+   
+    
+    
+    
+    @IBAction func actionButton(_ sender: UIButton) {
+        
+        guard textField.text?.isEmpty == false else { return }
+        
+        if let _ = Double(textField.text!) {
+            
+            let alert = UIAlertController(title: "Wrong format", message: "Please, enter your name", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
+            
+            print("Name format is wrong")
+        } else {
+            label.text = textField.text
+        }
 }
+         
+    
+    @IBAction func switchAction(_ sender: UISwitch) {
+        segmentControl.isHidden = !segmentControl.isHidden
+        label.isHidden = !label.isHidden
+        textField.isHidden = !textField.isHidden
+        slider.isHidden = !slider.isHidden
+        button.isHidden = !button.isHidden
+        datePicker.isHidden = !datePicker.isHidden
+        
+        if sender.isOn {
+            switchLabel.text = "Скрыть все элементы"
+        } else {
+            switchLabel.text = "Показать все элементы"
+        }
+    }
+    
+
+    
+
+}
+
+
+
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return uiElements.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return uiElements[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        selectedElement = uiElements[row]
+        textField.text = selectedElement
+        
+        switch row {
+        case 0:
+            hideAllElements()
+            segmentControl.isHidden = false
+        case 1:
+            hideAllElements()
+            label.isHidden = false
+        case 2:
+            hideAllElements()
+            slider.isHidden = false
+        case 3:
+            hideAllElements()
+        case 4:
+            hideAllElements()
+            button.isHidden = false
+        case 5:
+            hideAllElements()
+            datePicker.isHidden = false
+        default:
+            hideAllElements()
+        }
+    }
+    
+}
+
+
+
+
+
+
 
